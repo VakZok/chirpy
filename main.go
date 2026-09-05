cecbf3d5f085427a69124bfe41b26c3eadf7adce
VakZok <100447067+VakZok@users.noreply.github.com>
2026-09-05 13:38:04 +0200

    feat(handlers): implemented stateful handler for website visit counter

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(writer, request)
+	})
+}
+
+func (cfg *apiConfig) myMetricHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) myResetHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+}
+
+func main() {
+	myHandler := http.NewServeMux()
+	cfg := &apiConfig{}
+
+	// handling fileserver
+	fileServer := http.FileServer(http.Dir("."))
+	strippedPrefixFileServer := http.StripPrefix("/app", fileServer)
+	wrappedFileServer := cfg.middlewareMetricsInc(strippedPrefixFileServer)
+	myHandler.Handle("/app/", wrappedFileServer)
+
+	// handling healt data using anonymous function that returns handler function
+	myHandler.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte("OK"))
+	})
+
+	// handling website visit counter
+	myHandler.HandleFunc("/metrics", cfg.myMetricHandler)
+	myHandler.HandleFunc("/reset", cfg.myResetHandler)
+
+	// configuring http server
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: myHandler,
+	}
+
+	// start server and have it continously run listening for requests to serve
+	log.Fatal(s.ListenAndServe())
+}
