c77032ca6e66dd8be3011f89215c06f65b402e8f
VakZok <100447067+VakZok@users.noreply.github.com>
2026-09-08 21:45:07 +0200

    changed metric to admin version

--- main.go
+++ main.go
@@ -19,9 +19,16 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) myMetricHandler(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.Header().Set("Content-Type", "text/html")
 	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	writer.Write([]byte(fmt.Sprintf(
+		`<html>
+  			<body>
+     			<h1>Welcome, Chirpy Admin</h1>
+        		<p>Chirpy has been visited %d times!</p>
+          	</body>
+		</html>`, cfg.fileserverHits.Load(),
+	)))
 }
 
 func (cfg *apiConfig) myResetHandler(writer http.ResponseWriter, request *http.Request) {
@@ -49,7 +56,7 @@ func main() {
 
 	// handling website visit counter
 	myHandler.HandleFunc("GET /admin/metrics", cfg.myMetricHandler)
-	myHandler.HandleFunc("POST /api/reset", cfg.myResetHandler)
+	myHandler.HandleFunc("POST /admin/reset", cfg.myResetHandler)
 
 	// configuring http server
 	s := &http.Server{
