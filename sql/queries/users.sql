5be727009438ee64fcd560ec9ec00f0cef81a03c
VakZok <100447067+VakZok@users.noreply.github.com>
2026-09-16 17:04:33 +0200

    feat(db): add hashed_password column migration and query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,9 +1,10 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
 VALUES (
     gen_random_uuid(),
     NOW(),
     NOW(),
-    $1          -- email from client request
+    $1,         -- email from client request
+    $2          -- passworc from client request
 )
 RETURNING *;    -- return all values for http response
