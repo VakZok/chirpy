7254800402f319736c31eddd77164775f7fb9acb
VakZok <100447067+VakZok@users.noreply.github.com>
2026-09-19 13:42:54 +0200

    feat(auth): add JWT creation and validation
    Add MakeJWT and ValidateJWT functions to the auth package using
    golang-jwt/jwt/v5. MakeJWT signs a token with HS256 containing the
    user's ID as the subject, an issuer, and an expiration time.
    ValidateJWT parses and verifies the token signature, then extracts
    and returns the user ID from the claims.

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,12 @@
 package auth
 
 import (
+	"errors"
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 // https://pkg.go.dev/github.com/alexedwards/argon2id#section-readme
@@ -13,4 +18,65 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) (bool, error) {
 	match, err := argon2id.ComparePasswordAndHash(password, hash)
 	return match, err
-}	
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	createdAt := time.Now().UTC()
+	deleteAt := createdAt.Add(expiresIn)
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(createdAt),
+		ExpiresAt: jwt.NewNumericDate(deleteAt),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// convert string to byte-slice required by HS256 before passing it as key for signing
+	// see https://golang-jwt.github.io/jwt/usage/signing_methods/#signing-methods-and-key-types
+	key := []byte(tokenSecret)
+	signedToken, err := token.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claimsDestination := &jwt.RegisteredClaims{}
+
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		claimsDestination, // populate RegisteredClaims struct
+		// anonymous function to return the secret key used to verify the JWT signature
+		func(token *jwt.Token) (any, error) {
+			return []byte(tokenSecret), nil
+		},
+	)
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if !token.Valid {
+		return uuid.Nil, errors.New("Token invalid.")
+	}
+
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if subject == "" {
+		return uuid.Nil, errors.New("Missing subject.")
+	}
+
+	// convert subject string to uuid.UUID object
+	userID, err := uuid.Parse(subject)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userID, nil
+}
