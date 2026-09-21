572ae41490b8f263fc7e4661109c632adc147ab9
VakZok <100447067+VakZok@users.noreply.github.com>
2026-09-21 21:46:41 +0200

    feat(database): add refresh_tokens table migration

--- sql/schema/003_pwd.sql
+++ sql/schema/003_pwd.sql
@@ -1,9 +1,9 @@
--- +goose up
+-- +goose Up
 ALTER TABLE users
 ADD COLUMN hashed_password TEXT NOT NULL
 DEFAULT 'unset';
 
 
--- +goose down
+-- +goose Down
 ALTER TABLE users
 DROP COLUMN hashed_password;
